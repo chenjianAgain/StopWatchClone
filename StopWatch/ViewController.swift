513a6b7aca7255dfb5de262b8e943d142cd34858
陈健 <chenj12@zendaimoney.com>
2016-09-19 20:02:59 +0800

    Add stop watch logic.

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var time = 0
+    var timer = NSTimer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var timeLabel: UILabel!
 
+    @IBOutlet weak var stopBarButtonItem: UIBarButtonItem!
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        time = 0
+        
+        timeLabel.text = "0"
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.increaseTime), userInfo: nil, repeats: true)
+
+    }
+    
+    
+    @IBOutlet weak var play: UIBarButtonItem!
+    
+    func result() {
+        
+    }
+    
+    func increaseTime() {
+        time = time + 1
+        timeLabel.text = String(time)
+    }
 }
 
